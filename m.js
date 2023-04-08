8646a764ca549c4ec9708d7a94daee6646259e9f
Fire-45 <77906966+Fire-45@users.noreply.github.com>
2023-04-08 14:22:12 -0400

    Add files via upload

--- m.js
+++ m.js
@@ -1,9 +1,10 @@
 function Info(){
-    window.alert("Click on the 'begin' button to start the process of sound recognition and please remember to allow the computer to use you microphone");
+    window.alert("Click on the 'begin' button to start the process of sound recognition and please remember to allow the computer to use you microphone. PS It is a very fast process");
 }
 
 //Model 1 my voice only: https://teachablemachine.withgoogle.com/models/PwyTrZRSd/model.json
 //Model 2 my voice + Laila: https://teachablemachine.withgoogle.com/models/94aVY86qK/model.json
+//Model 3 my voice + Arnav: https://teachablemachine.withgoogle.com/models/yQCuakxIS/model.json
 
 var SpeechRecognition = window.webkitSpeechRecognition;
 var Recognition = new SpeechRecognition();
@@ -15,7 +16,7 @@ function BEGIN(){
     //after this, everything inside this Begin function is all code for the model + recognition commands
 
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/94aVY86qK/model.json",modelReady);
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/yQCuakxIS/model.json",modelReady);
     //this is to setup the model and make sure the microphone is working
 
     function modelReady(){
@@ -31,16 +32,22 @@ function BEGIN(){
         else{
             console.log(results)
             document.getElementById("WHO").innerHTML = results[0].label;
-            //label here in our case is the name of the voice
-
-            if(document.getElementById("SAID").innerHTML != ""){
-                classifier = "";
-            }
+            document.getElementById("CONFIDENCE").innerHTML = Math.floor(results[0].confidence * 100);
+            //label here in our case is the name of the voice + the confidence
+         
         }
 
         
     }
+
+    
 }
+
+
+function TIME(){
+    window.alert("Please check your results and reload the page afterwards");
+}
+//here I set a function connected to a timer in html, look in HTML
 
 Recognition.onresult = function(event){
     console.log(event);
